ffd37830dd373e2eebff5aefd0dfd624066bbeec
GolamShakir <47187160+GolamShakir@users.noreply.github.com>
2021-06-27 01:09:06 +0600

    Update app_server.pp

--- site/role/manifests/app_server.pp
+++ site/role/manifests/app_server.pp
@@ -2,8 +2,4 @@ class role::app_server {
   include profile::wed
   include profile::base
   include profile::app
-  include dockeragent
-  dockeragent::node { 'web.puppet.vm': }
-  dockeragent::node { 'db.puppet.vm': }
-  dockeragent::node { 'minetest.puppet.vm': }
 }
